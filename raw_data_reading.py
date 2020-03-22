c1a1a6ec0d32c0658900aad177d807fc99a1d81d
Nora Vrieler <neora.vrieler@mail.huji.ac.il>
2020-03-22 16:08:31 +0200

    continuing work on file-importing; fixing bugs in file-finding logic

--- raw_data_reading.py
+++ raw_data_reading.py
@@ -22,24 +22,10 @@ A Fixed-length signal set is acquired consecutively; it is a collection of trace
 and usually has some manipulation occurring at a fixed time from the start of the signals.
 
 
-Goals: 
-1. creating a CellName_RawData object that reads in all raw data traces of that neuron
-and stores them in homologous Neo objects regardless of which acquisition software was used.
-
-Procedure when reading in pClamp data:
-    - on a directory containing folders named CellName*, go into the relevant folder
-        *if the folder contains a dual-patch recording, files have to be separated accordingly
-    - get a list of all .abf-files in the folder
-    - for each file, read the raw data using Neo.AxonIO to get one Block per file.
-
-Procedure when reading in SutterPatch data:
-    - in a folder containing files named CellName.pxp, find the relevant one
-    - recreate the internal file structure to get paths to each data-containing subdirectory 
-    - for each subdirectory, read the raw data using Neo.IgorIO 
-    - create one Block per subdirectory, assigning* V and I to Segments and ChannelIndexes systematically
-        *each subdirectory contains a single signal only, so relationships between signals have to be assigned 'manually'
-        *to get GapFree traces from 'continuous recordings', traces have to be redimensioned. 
-
+Inventory of functions in this file/class:
+    1. get_singleneuron_rawdata for finding the path to the raw data file(s) of the neuron
+    2. files_reader - either abf or pxp, for actually reading the raw data and returning it in standardized format
+    3. functions for plotting and displaying summary information.
 
 2. writing functions for displaying useful summary information,
 that can be called on the raw data contained in the class instance
@@ -53,53 +39,71 @@ from neo import io
 
 # %% the SingleNeuron_RawData class definition:
 class SingleNeuron_RawData:
+    """
+    This class imports raw data I recorded using my conventions, independent of 
+    the software used for acquisition. 
+    
+    Some of the main differences between acquisition systems that my code accounts for:
+        
+    An singleneuron_name pxp-file recorded in SutterPatch corresponds to 
+    an singleneuron_name folder containing all recorded files for a single neuron in pClamp.
+    
+    The 'continuous-mode' recording setting in SutterPatch corresponds to 
+    'gap-free' mode in pClamp, though data have to be redimensioned (from matrix to single trace) 
+    to look the same.
+    
+    Grouping of signals and assignment of channel_indexes
+    is built into Neo/AxonIO to happen systematically per file,
+    whereas Neo/IgorIO reads only individual traces and requires grouping into signals
+    following my conventions.
+    """
     
     # init
     def __init__(self, singleneuron_name, path="D:\\hujigoogledrive\\research_YaromLabWork\\data_elphys_andDirectlyRelatedThings\\olive\\elphysData_recordedByMe"):
+    # path should be to a folder containing sub-folders containing raw data.
+    # path gets updated to absolute path of the folder/file containing the raw data recorded for singleneuron
         self.name = singleneuron_name
+        self.file_path = path #gets updated by get_singleneuron_rawdata
+        self.type = None #raw data file(s) type; gets updated once data files are found
         self.rawdata_blocks = []
-        self.path = path
-        self.type = None
-        self.find_cell_path()
+        self.get_singleneuron_rawdata()
     
-    # using neuron_name to navigate to the right raw data file(s), deducing whether it's gonna be abf or pxp/ibw)
-    def find_cell_path(self):
-        for item in os.listdir(self.path):
-            for filename in os.listdir(self.path + '\\' + item):
-                filename_split = filename.split('.')
-                if len(filename_split) == 2 and filename_split[0] == self.name and filename_split[1] == 'pxp':
-                    self.type = 'pxp'
-                    
-                elif len(filename_split) == 1 and filename_split[0] == self.name:
-                    self.type = 'abf' 
-                    
-                else:
-                    continue
-                self.path += f'\\{item}\\{filename}'
-                break
-            
+    def get_singleneuron_rawdata(self):    
+    # this function uses neuron_name to find a path to the raw data file(s) recorded for that neuron;
+    # once the right path(s) are found, it calls on files_reader to read the data in as Neo blocks.
+    # !! it currently only works for single-cell recordings in abf format !!
+        for folder_name in os.listdir(self.file_path):
+            subdirectory_path = self.file_path + '\\' + folder_name
+            if self.name in os.listdir(subdirectory_path):
+                self.type = 'abf'
+                self.file_path = subdirectory_path+'\\'+self.name
+                self.rawdata_blocks = self.files_reader_abf()
+            elif (self.name + '.pxp') in os.listdir(subdirectory_path):
+                self.type = 'pxp'
+                self.file_path = subdirectory_path
+                self.rawdata_blocks = self.files_reader_pxp()
+            else:
+                continue
+            break
         if not self.type:
-            print('No file matching filename found')
-            exit
-        elif self.type == 'abf':
-            self.files_reader_abf()
-        elif self.type == 'pxp':
-            self.files_reader_pxp()
+            print('files matching neuron name exactly were not found')
+        
             
-                
-                
-    
+
     # the actual reading in of raw data from files
     def files_reader_abf(self):
-        """Takes a list of abf-file names; returns the recorded data as a list of neo blocks.
-        filenames_list should be a list of abf-file names on the current directory path.
+        """This function changes the current directory to the folder containing
+        the .abf raw data files recorded for singleneuron, and returns the 
+        recorded data as a list of neo blocks.
         
         reading abf-files using Neo.io:
-        each abf-file contains either a single set (V and I) of GapFree signals, 
-        or sets of Fixed-length signals.
-        Information on segments and channel_indexes is read from the abf file directly by AxonIO. 
+        by my convention, files are recorded either in Gap-free mode or in Fixed-length mode.
+        A file recorded in Gap-free mode contains a single set (V and I) of signals (of indefinite length)
+        and is read by Neo.AxonIO as a single segment with two channel_indexes (V and I).
+        A file recorded in Fixed-length mode contains multiple sets (V, I and possibly other) signals (of fixed length)
+        and is read by Neo.AxonIO as consecutive segments, each with two or more channel_indexes (V, I and other [usually TTL]).
         """
-        os.chdir(self.path)
+        os.chdir(self.file_path)
         for file in os.listdir():
             if file.endswith(".abf"):
                 reader = io.AxonIO(filename=file)
@@ -111,10 +115,12 @@ class SingleNeuron_RawData:
                 self.rawdata_blocks.append(block)
     
     def files_reader_pxp(self):
-        """Takes a list of pxp-file subdirectory names; returns the recorded data as a list of neo blocks.
-        directorynames_list should be a list of pxp-subdirectory names existing inside the pxp-file.
+        """This function changes the current directory to the folder containing
+        the .pxp raw data file recorded for singleneuron, and returns the 
+        recorded data as a list of neo blocks.
         
-        reading pxp-files using Neo.io: 
+        reading pxp-files using Neo.io:
+        by my convention, files are recorded with 'consecutive-mode' on or off.
         each pxp-subdirectory contains a single signal (either V, I or other AuxIn);
         it is stored in a matrix form, with each row (?!!) in the matrix representing a consecutive segment.
         GapFree signals are constructed by redimensioning matrices of data acquired in 'continuous mode'.
@@ -125,4 +131,6 @@ class SingleNeuron_RawData:
         Each subdirectory name starts with a protocol (P) number and name.
         
         """
-        
+        os.chdir(self.file_path)
+        file_name = self.file_path+'\\'+self.name+'.pxp'
+        reader = io.IgorIO(filename=file_name)
---
+++ scrappad_script_singleneuron_analyses.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 22 11:21:16 2020
+
+@author: neert
+"""
+import os
+os.chdir("D:\\hujigoogledrive\\research_YaromLabWork\\Code_inPython\\elphysDataAnalyses_working")
+from raw_data_reading import SingleNeuron_RawData
+# %%
+emptycell_RawData = SingleNeuron_RawData("20190814")
+cell20190814A_RawData = SingleNeuron_RawData("20190814A")
+# %%
+cell20200310F_RawData = SingleNeuron_RawData("20200310F")

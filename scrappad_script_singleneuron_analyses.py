606ab237fb22a46e6407bacd2cacc865720674b9
Nora Vrieler <neora.vrieler@mail.huji.ac.il>
2020-05-06 15:11:35 +0300

    playing around with plotting things in scrappad

--- scrappad_script_singleneuron_analyses.py
+++ scrappad_script_singleneuron_analyses.py
@@ -18,8 +18,8 @@ from singleneuron_class import SingleNeuron
 from singleneuron_analyses_functions import get_depolarizingevents
 # %% importing of a couple of 'best representative' recordings
 cell20190805A = SingleNeuron('20190805A')
-cell20190805A.rawdata_remove_nonrecordingblock('gapFree_0000.abf')
-cell20190805A.rawdata_remove_nonrecordingchannel('gapFree_0001.abf', 1)
+# cell20190805A.rawdata_remove_nonrecordingblock('gapFree_0000.abf')
+# cell20190805A.rawdata_remove_nonrecordingchannel('gapFree_0001.abf', 1)
 # cell20190805A.plot_allrawdata()
 
 # a_segment = cell20190805A.rawdata_blocks[0].segments[0].time_slice(
@@ -31,20 +31,36 @@ cell20190805A.rawdata_remove_nonrecordingchannel('gapFree_0001.abf', 1)
 #                                              peakwindow=7,
 #                                              spikeahpwindow=250, plot='on')
 
-cell20190805A.get_depolarizingevents_fromrawdata(oscfilter_lpfreq=25,
-                                                 peakwindow=7,
-                                                 spikeahpwindow=250)
-cell20190805A.write_results()
+# cell20190805A.get_depolarizingevents_fromrawdata(oscfilter_lpfreq=25,
+#                                                  peakwindow=7,
+#                                                  spikeahpwindow=250)
+# cell20190805A.write_results()
 
+cell20190805A.depolarizing_events.hist(column=['amplitude', 'rise_time', 'half_width'],
+                                       bins=50)
+# %%
+cell20190805A.depolarizing_events.plot.scatter(x='rise_time', y='amplitude',
+                                               c='edtrace_rise_time',colormap='viridis')
+cell20190805A.depolarizing_events.plot.scatter(x='edtrace_rise_time',y='edtrace_amplitude',
+                                               c='rise_time', colormap='viridis')
 
+# %%
 cell20190814A = SingleNeuron("20190814A")
-cell20190814A.rawdata_remove_nonrecordingchannel('gapFree_0000.abf', 2)
+# cell20190814A.rawdata_remove_nonrecordingchannel('gapFree_0000.abf', 2)
 # a_segment = cell20190814A.rawdata_blocks[3].segments[0].time_slice(t_start=150*pq.s,
 #                                                                    t_stop=200*pq.s)
 # apsdict, depolsdict = get_depolarizingevents(a_segment, plot='on')
-cell20190814A.get_depolarizingevents_fromrawdata()
-cell20190814A.write_results()
+# cell20190814A.get_depolarizingevents_fromrawdata()
+# cell20190814A.write_results()
+cell20190814A.depolarizing_events.plot.scatter(x='rise_time', y='amplitude',
+                                               c='half_width', colormap='viridis')
+cell20190814A.depolarizing_events.plot.scatter(x='edtrace_rise_time', y='amplitude',
+                                               c='edtrace_half_width', colormap='viridis')
+cell20190814A.depolarizing_events.plot.scatter(x='edtrace_rise_time', y='edtrace_amplitude',
+                                               c='edtrace_half_width', colormap='viridis')
 
+
+# %%
 
 cell20190729A = SingleNeuron("20190729A")
 # a_segment = cell20190729A.rawdata_blocks[1].segments[0].time_slice(t_start=500*pq.s,
@@ -52,21 +68,54 @@ cell20190729A = SingleNeuron("20190729A")
 
 # apsdict,depolsdict = get_depolarizingevents(a_segment, spikeahpwindow=250, plot='on')
 
-cell20190729A.get_depolarizingevents_fromrawdata(spikeahpwindow=250)
-cell20190729A.write_results()
+# cell20190729A.get_depolarizingevents_fromrawdata(spikeahpwindow=250)
+# cell20190729A.write_results()
+cell20190729A.depolarizing_events.plot.scatter(x='rise_time', y='amplitude',
+                                               c='half_width', colormap='viridis')
+cell20190729A.depolarizing_events.plot.scatter(x='edtrace_rise_time', y='amplitude',
+                                               c='edtrace_half_width', colormap='viridis')
+cell20190729A.depolarizing_events.plot.scatter(x='edtrace_rise_time', y='edtrace_amplitude',
+                                               c='edtrace_half_width', colormap='viridis')
 
-
+# %%
 cell20200308F = SingleNeuron('20200308F')
-cell20200308F.get_depolarizingevents_fromrawdata(spikeahpwindow=600)
-cell20200308F.write_results()
+# cell20200308F.get_depolarizingevents_fromrawdata(spikeahpwindow=600)
+# cell20200308F.write_results()
+cell20200308F.depolarizing_events.plot.scatter(x='rise_time', y='amplitude',
+                                               c='half_width', colormap='viridis')
+cell20200308F.depolarizing_events.plot.scatter(x='edtrace_rise_time', y='amplitude',
+                                               c='edtrace_half_width', colormap='viridis')
+cell20200308F.depolarizing_events.plot.scatter(x='edtrace_rise_time', y='edtrace_amplitude',
+                                               c='edtrace_half_width', colormap='viridis')
 
+
+# %%
 cell20200308D = SingleNeuron('20200308D')
-cell20200308D.get_depolarizingevents_fromrawdata(spikeahpwindow=200)
-cell20200308D.write_results()
+# cell20200308D.get_depolarizingevents_fromrawdata(spikeahpwindow=200)
+# cell20200308D.write_results()
+cell20200308D.depolarizing_events.plot.scatter(x='rise_time', y='amplitude',
+                                               c='half_width', colormap='viridis')
+cell20200308D.depolarizing_events.plot.scatter(x='edtrace_rise_time', y='amplitude',
+                                               c='edtrace_half_width', colormap='viridis')
+cell20200308D.depolarizing_events.plot.scatter(x='edtrace_rise_time', y='edtrace_amplitude',
+                                               c='edtrace_half_width', colormap='viridis')
 
+cell20200308D.depolarizing_events.hist(column=['amplitude','rise_time','half_width',
+                                               'edtrace_amplitude','edtrace_rise_time','edtrace_half_width'],
+                                       bins=50)
+
+# %%
 cell20200310C = SingleNeuron('20200310C')
-cell20200310C.get_depolarizingevents_fromrawdata()
-cell20200310C.write_results()
+# cell20200310C.get_depolarizingevents_fromrawdata()
+# cell20200310C.write_results()
+cell20200310C.depolarizing_events.plot.scatter(x='rise_time', y='amplitude',
+                                               c='half_width', colormap='viridis')
+cell20200310C.depolarizing_events.plot.scatter(x='edtrace_rise_time', y='amplitude',
+                                               c='edtrace_half_width', colormap='viridis')
+cell20200310C.depolarizing_events.plot.scatter(x='edtrace_rise_time', y='edtrace_amplitude',
+                                               c='edtrace_half_width', colormap='viridis')
+
+
 # %% plotting some stuff
 cell20190805A.plot_depolevents_overlayed(get_subthreshold_events=False,
                                          colorby_measure='baselinev')
@@ -80,7 +129,7 @@ cell20190805A.plot_individualdepolevents_withmeasures((cell20190805A.depolarizin
                                          (cell20190805A.depolarizing_events['edtrace_rise_time'] < 2))
 # %%
 cell20190805A.plot_depolevents_overlayed((cell20190805A.depolarizing_events.amplitude > 10) &
-                                         (cell20190805A.depolarizing_events.rise_time < 2),
+                                         (cell20190805A.depolarizing_events.edtrace_rise_time < 2),
                                          colorby_measure='applied_current')
 
 cell20190805A.plot_depolevents_overlayed((cell20190805A.depolarizing_events.amplitude < 5) &
@@ -91,7 +140,7 @@ cell20190805A.plot_depolevents_overlayed((cell20190805A.depolarizing_events.ampl
 # cell20190805A.plot_individualdepolevents_withmeasures(
 #     cell20190805A.depolarizing_events.amplitude > 20)
 
-cell20190805A.plot_individualdepolevents_withmeasures(get_subthreshold_events=False)
+# cell20190805A.plot_individualdepolevents_withmeasures(get_subthreshold_events=False)
 
 # %%
 cell20190814A.plot_depolevents_overlayed(get_subthreshold_events=False,

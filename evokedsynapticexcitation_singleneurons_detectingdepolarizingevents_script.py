cc9e60fe9a627745bf7c9998ce5eb19e09cd8752
Nora Vrieler <neora.vrieler@mail.huji.ac.il>
2020-05-28 21:23:32 +0300

    ongoing work on analyzing depolarizing events in neurons with evoked synaptic responses

---
+++ evokedsynapticexcitation_singleneurons_detectingdepolarizingevents_script.py
@@ -0,0 +1,17 @@
+from singleneuron_class import SingleNeuron
+import matplotlib.pyplot as plt
+import quantities as pq
+
+# %% experiment: ChR activation in Thy1 mouse
+cell20190527A = SingleNeuron('20190527A')
+
+cell20190527C = SingleNeuron('20190527C')
+
+cell20190529B = SingleNeuron('20190529B')
+
+cell20190529C = SingleNeuron('20190529C')
+
+cell20190529D = SingleNeuron('20190529D')
+
+cell20190529E = SingleNeuron('20190529E')
+

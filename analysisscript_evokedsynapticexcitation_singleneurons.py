873d3d5eaf3ee58ff9e141707fad744991708203
Nora Vrieler <neora.vrieler@mail.huji.ac.il>
2020-06-17 21:25:17 +0300

    continuing analyses of neuron20190527; organizing narrowing down fast-events criteria

--- analysisscript_evokedsynapticexcitation_singleneurons.py
+++ analysisscript_evokedsynapticexcitation_singleneurons.py
@@ -1,59 +1,109 @@
-# %% subthreshold depolarizations and action potentials
-# Q1: do they occur spontaneously?
-# if yes, with what frequency?
+# %% putative axonal spine responses in IO neurons.
+# focus of this investigation: fast, depolarizing events of highly consistent waveform (as reflected by near-identical
+# rise-time and half-width), that fall into groups of different specific amplitudes.
 
-# Q2: are they among the light-evoked responses of the neuron?
-# if yes, with what frequency do they occur?
+# Q1: can (a) group(s) of such fast-events be identified in the neuron's ongoing spontaneously occurring activity?
+# if yes: what are the rise_time / half_width / amplitude parameters that define these events in this neuron?
 
-# Q3: what portion of APs is evoked from a fast-event? (both for spontaneous and evoked events)
+# Q2: are fast-events included in the neuron's response to synaptic inputs?
+# if yes: how reliably are they evoked? Does it depend on light intensity?
 
-# Q4: (how) do the answers to Q1-3 depend on the baseline voltage?
-# a] do spontaneous APs and fast-events occur more frequently with more depolarized V?
-# b] are they evoked more often with more depolarized V?
-# c] if we count fast-events + APs evoked from fast-events, is their overall frequency together
-#       the same regardless of baseline voltage?
-# d] are fast-events more likely to evoke an AP at more depolarized Vrest?
+# Q3: what is the effect of baselinev on the appearance of fast-events?
+# if they indeed represent APs triggered by synaptic inputs in axonal spines, then
+# - reducing baselinev should increase the occurrence of large-amplitude fast-events (since they no longer trigger APs)
+# - increasing baselinev should increase the overall frequency of fast-events and fast-event-triggered APs
 
 # %% imports
 from singleneuron_class import SingleNeuron
 import matplotlib.pyplot as plt
 # %% depolarizing events analyses - cell20190527A
 cell20190527Arawdata = SingleNeuron('20190527A')
-# %% subthreshold depolarizations
-# scatter of amp. vs rise-time to see whether there are (groups of)
-# depolarizing events that satisfy fast-events criteria;
-# separate plots for light-evoked and spontaneous events
 cell20190527Aevoked_events = cell20190527Arawdata.depolarizing_events.applied_ttlpulse
 cell20190527Aspont_events = ~cell20190527Arawdata.depolarizing_events.applied_ttlpulse
-
-# cell20190527Arawdata.scatter_depolarizingevents_measures('amplitude', 'rise_time',
-#                                                          cmeasure='baselinev',
-#                                                          spontevents=cell20190527Aspont_events,
-#                                                          evokedevents=cell20190527Aevoked_events)
+# %% scatter plot: amplitude vs rise-time
+cell20190527Arawdata.scatter_depolarizingevents_measures('amplitude', 'rise_time_20_80',
+                                                         cmeasure='half_width',
+                                                         spontevents=cell20190527Aspont_events,
+                                                         evokedevents=cell20190527Aevoked_events)
 # plt.suptitle('all detected events')
 
-# histogram of baselinev's
-# cell20190527Arawdata.depolarizing_events.hist('baselinev',bins=80)
-
+# %% scatter plot: half-width vs rise-time
+cell20190527Arawdata.scatter_depolarizingevents_measures('half_width', 'rise_time_20_80',
+                                                         cmeasure='amplitude',
+                                                         spontevents=cell20190527Aspont_events)
+cell20190527Arawdata.scatter_depolarizingevents_measures('half_width', 'rise_time_20_80',
+                                                         cmeasure='amplitude',
+                                                         evokedevents=cell20190527Aevoked_events)
+# %% excluding groups of events that definitely do not meet criteria
 # events with baselinev > -25mV are in fact spike-shoulder peaks:
 spikeshoulderpeaks = cell20190527Arawdata.depolarizing_events.baselinev > -25
 # cell20190527Arawdata.plot_depolevents_overlayed(spikeshoulderpeaks,
 #                                                 colorby_measure='baselinev',
 #                                                 prealignpoint_window_inms=20)
-# updating events-tables accordingly.
+# updating events-tables accordingly:
 cell20190527Aevoked_events = cell20190527Aevoked_events \
                              & (~spikeshoulderpeaks)
 cell20190527Aspont_events = cell20190527Aspont_events \
                             & (~spikeshoulderpeaks)
-# %% trying to find further criteria for fast-events:
-cell20190527Arawdata.scatter_depolarizingevents_measures('amplitude', 'rise_time',
-                                                         cmeasure='baselinev',
-                                                         spontevents=cell20190527Aspont_events,
-                                                         evokedevents=cell20190527Aevoked_events)
-plt.suptitle('all detected subthreshold events')
-# although it is clear from the amp/rise-time scatter that there are fast-events,
-# it shows no clear boundary between these and spikelets. In fact, there seems to be also
-# other kinds of relatively fast and large events.
+
+# both in spont. and evoked events there is one single one with a much larger amplitude than all the others
+# the evoked one looks like it could be real, though it's coming from baselinev ~-30;
+# the spont. one looks like a weird noise
+verylargeampevents = (cell20190527Arawdata.depolarizing_events.amplitude > 15) & (~spikeshoulderpeaks)
+# cell20190527Arawdata.plot_depoleventsgroups_overlayed((verylargeampevents&cell20190527Aspont_events),
+#                                                       (verylargeampevents&cell20190527Aevoked_events),
+#                                                       group_labels=['spont', 'evoked'],
+#                                                       plt_title='strangely large-amp events')
+# updating events-tables accordingly:
+cell20190527Aspont_events = cell20190527Aspont_events \
+                            & (~verylargeampevents)
+
+# 2mV is the smallest amplitude at which a somewhat clear grouping of fast rise_time (20_80 < 0.6 ms) events
+# can be seen in the rise-time/amplitude scatter of spontaneous events
+# TODO: revisit this criterion to see if there are more fast-events to be fished out of the small depolarizations
+# updating events-tables accordingly:
+amptoosmall = cell20190527Arawdata.depolarizing_events.amplitude < 2
+cell20190527Aevoked_events = cell20190527Aevoked_events \
+                             & (~amptoosmall)
+cell20190527Aspont_events = cell20190527Aspont_events \
+                            & (~amptoosmall)
+
+# the string of spontaneous events in different amplitude-groups all seems to have rise_time_20_80 < 1ms
+# in the evoked responses, there may be some that are a spikelet+fast-event;
+# in the spontaneous responses there are some intriguing large-amplitude (>5mV) ones that definitely aren't fast-events
+# TODO: revisit this criterion to include evoked responses that are fast-event + something
+risetimetoolong = cell20190527Arawdata.depolarizing_events.rise_time_20_80 > 1
+# cell20190527Arawdata.plot_depolevents_overlayed((cell20190527Aspont_events & risetimetoolong),
+#                                                 colorby_measure='baselinev',
+#                                                 do_baselining=True,
+#                                                 # do_normalizing=True,
+#                                                 total_plotwindow_inms=15
+#                                                 )
+# plt.suptitle('>1ms rise-time_20_80 events, spontaneous only')
+# cell20190527Arawdata.plot_depolevents_overlayed((cell20190527Aevoked_events & risetimetoolong),
+#                                                 colorby_measure='baselinev',
+#                                                 do_baselining=True,
+#                                                 # do_normalizing=True,
+#                                                 total_plotwindow_inms=15
+#                                                 )
+# plt.suptitle('>1ms rise-time_20_80 events, evoked only')
+# updating events-tables accordingly:
+cell20190527Aevoked_events = cell20190527Aevoked_events \
+                             & (~risetimetoolong)
+cell20190527Aspont_events = cell20190527Aspont_events \
+                            & (~risetimetoolong)
+
+# %%
+
+
+
+
+
+
+
+
+
+
 # %%
 # first, let's see the large-amplitude events and yet do not have very fast rise-time:
 largeampevents = (cell20190527Arawdata.depolarizing_events.amplitude > 10) \
@@ -73,7 +123,8 @@ cell20190527Aspont_events = cell20190527Aspont_events & (cell20190527Arawdata.de
 #
 # let's see if this is also the case for other relatively large-amp but slow rise events:
 bigbutslowevents = (cell20190527Arawdata.depolarizing_events.amplitude > 2) \
-                   & (cell20190527Arawdata.depolarizing_events.rise_time_20_80 > 1.5)
+                   & (cell20190527Arawdata.depolarizing_events.rise_time_20_80 > 1.5) \
+                   & (cell20190527Arawdata.depolarizing_events.baselinev < -45)
 cell20190527Arawdata.plot_depolevents_overlayed((cell20190527Aspont_events&bigbutslowevents),
                                                 colorby_measure='baselinev',
                                                 do_baselining=True,
@@ -85,9 +136,9 @@ plt.suptitle('big yet relatively slow events')
 # yea pretty much looks like exactly that, every one of these events looks compound, though it doesn't seem like
 # each of these necessarily has a fast-event in there...
 # %% now let's see things that really should be fast-events
-probablyfastevents = (cell20190527Arawdata.depolarizing_events.amplitude > 2) \
+probablyfastevents = (cell20190527Arawdata.depolarizing_events.amplitude > 3) \
                      & (cell20190527Arawdata.depolarizing_events.rise_time_20_80 < 1.5) \
-                     & (cell20190527Arawdata.depolarizing_events.baselinev < -45)  # just to see fewer lines
+                     & (cell20190527Arawdata.depolarizing_events.baselinev < -50)  # just to see fewer lines
 cell20190527Arawdata.plot_depolevents_overlayed((cell20190527Aspont_events&probablyfastevents),
                                                 colorby_measure='baselinev',
                                                 do_baselining=True,
@@ -101,9 +152,10 @@ plt.suptitle('should-be fast-events')
 # too large to be a spikelet but just a bit too slow to be a fast-event, and from the line plot it looks like
 # this event also comes in amplitude =~2mV.
 # %% narrowing it down to things that are definitely fast-events
-definitelyfastevents = (cell20190527Arawdata.depolarizing_events.amplitude > 2) \
-                     & (cell20190527Arawdata.depolarizing_events.rise_time_20_80 <= 0.6)
-cell20190527Arawdata.plot_depolevents_overlayed((cell20190527Aspont_events&definitelyfastevents),
+definitelyfastevents = (cell20190527Arawdata.depolarizing_events.amplitude > 4) \
+                       & (cell20190527Arawdata.depolarizing_events.rise_time_20_80 <= 0.7) \
+                       & (cell20190527Arawdata.depolarizing_events.half_width < 4)
+cell20190527Arawdata.plot_depolevents_overlayed((cell20190527Aevoked_events&definitelyfastevents),
                                                 colorby_measure='baselinev',
                                                 do_baselining=True,
                                                 # do_normalizing=True,
@@ -112,3 +164,13 @@ cell20190527Arawdata.plot_depolevents_overlayed((cell20190527Aspont_events&defin
                                                 timealignto_measure='rt_midpoint_idx'
                                                 )
 plt.suptitle('fast-events only')
+# %% find amplitude, rise-time and half-width criteria for each neuron
+# - since fast-events have the same shape when normalized, they should also all have the same half-width
+# frequency dependence on membrane potential
+# evoked groups dependence on membrane potential (when low, we'll see big events that at high vrest become spikes)
+cell20190527Arawdata.scatter_depolarizingevents_measures('amplitude', 'half_width',
+                                                         cmeasure='rise_time_20_80',
+                                                         spontevents=(cell20190527Aevoked_events&definitelyfastevents))
+
+# doublets: possibly one of the axonal spines is capable of activating the AIS
+# should always see the same two amplitude events stacked together.

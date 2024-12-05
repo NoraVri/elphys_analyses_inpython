89826c5f097f4855bfacb8ea27860a18b4a80e60
Nora Vrieler <wgo5481@fsm.northwestern.edu>
2024-12-05 12:07:27 -0600

    ongoing work analyzing STNstim-SNrrec data

--- 240530Y_script_optoresponses_sthr.py
+++ 240530Y_script_optoresponses_sthr.py
@@ -20,10 +20,76 @@ neuron_data.plot_rawdatablocks(segments_overlayed=True)
 # then stabilizing for a bit before changing again suddenly during second recording block; then APcurrents start to
 # appear as rebound-responses to voltage step. Clamp clearly bad at this point, reflected in voltage measurement.
 
+# %% getting optoStim response measurements:
+neuron_data.get_singlepulse_ttlonmeasures_fromrawdata(response_window_inms=50)
+ttlonmeasures = neuron_data.ttlon_measures.copy()
 
+# check and see recording conditions:
+sns.jointplot(data=ttlonmeasures, x='applied_voltage', y='baselinec', hue='file_origin')
+# gradual shift down in holdingC stabilizing for the last two recording blocks - looks like it could be
+# consistent with drug effect
 
+# Let's see some more response parameters:
+ttlonmeasures.plot.scatter('response_maxamp_postttl_t_inms', 'response_maxamp', c='baselinec', colormap='Spectral')
+ttlonmeasures.hist(column='response_maxamp_postttl_t_inms', bins=200)
+ttlonmeasures.hist(column='baselinec_range', bins=200)
+# Looks like all except 2 responses to optostim are clustered very tightly together in measured parameters
+# One response measured as peaking ~80ms post ttl (all others <10ms) is because of some weird noise-thing
+# in the recording at that time; the actual response looks good though.
+## Re-running get_singlepulse_ttlonmeasures_fromrawdata with 50ms time window to avoid this
+# This looks good: by amplitude, one response was an APcurrent (amp~8nA, all other response amps <600pA).
 
+# %% Getting subthreshold responses only:
+ttlonmeasures_sthr = ttlonmeasures[ttlonmeasures.response_maxamp < 1000]
 
+# %% examining outliers and excluding bad measurements from the data:
+ttlonmeasures_sthr.plot.scatter('response_maxamp_postttl_t_inms', 'response_maxamp', c='baselinec', colormap='Spectral')
+ttlonmeasures_sthr.plot.scatter('response_maxamp_postttl_t_inms', 'baselinec_range', c='baselinec', colormap='Spectral')
+ttlonmeasures_sthr.hist(column='response_maxamp_postttl_t_inms', bins=200)
+ttlonmeasures_sthr.hist(column='baselinec_range', bins=200)
+# Looks like there aren't really outliers in this data.
 
+for file in ttlonmeasures_sthr.file_origin.unique():
+    neuron_data.plot_ttlaligned(ttlonmeasures_sthr[ttlonmeasures_sthr.file_origin == file],
+                            postttl_t_inms=25,
+                            prettl_t_inms=5,
+                            )
+# Interesting: all responses seem to have a double up-stroke, with a plateau after the first that is much
+# more consistent in amplitude than the peak that follows the second up-stroke (and the plateau-level may be different
+# between drug conditions).
+
+# Having looked at all the data, I am convinced that the response peaks were all measured appropriately.
+
+# This dataset consists of 499 recorded subthreshold responses.
+
+# %% adding optoStim parameters to the dataframe:
+# Light stim duration was always 0.5ms;
+# Drug applied: Sulpride;
+# Light intensity was always the same (%intensity not noted)
+
+# Adding drug conditions as a column to ttlonmeasures dataframe: (from Notes0530.rtf, cell 1)
+drug_conditions = ['no drug', 'wash in/with drug', 'with drug', 'wash out']
+ttlonmeasures_sthr.loc[:, 'drug_condition'] = 'no drug'
+ttlonmeasures_sthr.loc[ttlonmeasures_sthr.file_origin.str.contains('0001'), 'drug_condition'] = 'wash in/with drug' #!Note: blocks marked as having drug applied start at the time solution is switched, so the first ... segments should be considered 'wash in'.
+ttlonmeasures_sthr.loc[ttlonmeasures_sthr.file_origin.str.contains('0002'), 'drug_condition'] = 'with drug'
+ttlonmeasures_sthr.loc[ttlonmeasures_sthr.file_origin.str.contains('0003'), 'drug_condition'] = 'wash out'
+ttlonmeasures_sthr.loc[ttlonmeasures_sthr.file_origin.str.contains('0004'), 'drug_condition'] = 'wash out'
+
+# plotting a quick overview of the recording conditions:
+sns.scatterplot(data=ttlonmeasures_sthr, x='applied_voltage', y='baselinec',
+                hue='drug_condition',)
+
+# %% plots: response sensitivity to drug
+sns.lmplot(data=ttlonmeasures_sthr, x='baselinec', y='response_maxamp', hue='drug_condition',)
+plt.figure()
+sns.boxplot(data=ttlonmeasures_sthr, x='ttlon_duration_inms', y='response_maxamp', hue='drug_condition')
+
+sns.lmplot(data=ttlonmeasures_sthr, x='response_maxamp', y='response_maxamp_postttl_t_inms', hue='drug_condition',)
+plt.figure()
+sns.boxplot(data=ttlonmeasures_sthr, x='ttlon_duration_inms', y='response_maxamp_postttl_t_inms', hue='drug_condition')
+# This all looks like drug has no effect at all.
+
+# %% saving the data: subthreshold responses to optoStim
+neuron_data.write_df_tocsv(ttlonmeasures_sthr, 'optostimresponses_sthr')
 
 

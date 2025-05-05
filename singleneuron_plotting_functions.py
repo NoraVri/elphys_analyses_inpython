683808d1f2183ae9b613dd329bd7fac0e9eb1d3b
Nora Vrieler <wgo5481@fsm.northwestern.edu>
2025-05-04 19:06:27 -0500

    another small bug fix to plotting code

--- singleneuron_plotting_functions.py
+++ singleneuron_plotting_functions.py
@@ -146,8 +146,8 @@ def qad_basic_plot_for_spike_detection(primary_recording_unit,
                         data_trace[data_trace_pastthreshold_idcs],
                         color='y', label='spikepeaktrace-points')
     if (len(peaks_idcs) >= 1):
-        axes[0].scatter(time_axis[data_trace_pastthreshold_idcs],
-                        spikedetection_data_trace[data_trace_pastthreshold_idcs],
+        axes[0].scatter(time_axis[peaks_idcs],
+                        spikedetection_data_trace[peaks_idcs],
                         color='b', label='detected peaks')
         axes[1].scatter(time_axis[peaks_idcs], data_trace[peaks_idcs],
                         color='b', label='detected peaks')
